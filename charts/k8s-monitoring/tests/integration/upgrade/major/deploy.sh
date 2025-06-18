8f2a391111aa779cf05f0cc2799e788ca7e2217c
Pete Wall <pete.wall@grafana.com>
2025-06-18 12:58:57 -0500

    Update alloy operator and fix major and minor upgrade tests
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- charts/k8s-monitoring/tests/integration/upgrade/major/deploy.sh
+++ charts/k8s-monitoring/tests/integration/upgrade/major/deploy.sh
@@ -8,7 +8,7 @@ set -eo pipefail
 CLUSTER_NAME=$(yq eval '.cluster.name' values.yaml)
 CURRENT_VERSION="$(yq eval '.version' ../../../../Chart.yaml)"
 IFS='.' read -r major minor patch <<< "${CURRENT_VERSION}"
-PREVIOUS_MAJOR_RELEASE="${major - 1}"
+PREVIOUS_MAJOR_RELEASE="$((major - 1))"
 
 echo "Installing version ${PREVIOUS_MAJOR_RELEASE}..."
 helm upgrade --install k8smon --version "${PREVIOUS_MAJOR_RELEASE}" --repo https://grafana.github.io/helm-charts k8s-monitoring -f "${TEST_DIR}/values.yaml" --set "cluster.name=${CLUSTER_NAME}" --set "clusterMetrics.opencost.opencost.exporter.defaultClusterId=${CLUSTER_NAME}" --wait
